4d9a2df7fc9a9c9b4b94386421b37ed9375522b2
Yussel Rosario <yusselrosario@gmail.com>
2016-12-15 12:39:44 -0500

    add configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.smtp_settings = {
+     address: "smtp.gmail.com",
+     port: 587,
+     domain: ENV["DOMAIN_NAME"],
+     authentication: "plain",
+     enable_starttls_auto: true,
+     user_name: ENV["GMAIL_USERNAME"],
+     password: ENV["GMAIL_PASSWORD"]
+}
+
+# Send email in development mode.
+  config.action_mailer.perform_deliveries = true
 end
